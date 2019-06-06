6cf9c462f5087778c9b20558885cffddbb933092
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2019-06-06 15:49:28 -0500

    Add some plist files for signing

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -9,53 +9,53 @@ import colors from '../constants/colors';
 library.add(faTimes);
 
 const Toast = styled(animated.div)`
-    position: fixed;
-    bottom: 10px;
-    right: 10px;
-    margin-left: 10px;
-    width: auto;
-    min-height: fit-content;
-    max-height: 200px;
-    height: 40px;
-    border: 1px solid ${colors.primary};
-    padding: 5px;
-    background-color: ${colors.light_grey};
-    border-radius: 3px;
-    align-items: center;
-    display: flex;
-    font-size: 1.2em;
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  margin-left: 10px;
+  width: auto;
+  min-height: fit-content;
+  max-height: 200px;
+  height: 40px;
+  border: 1px solid ${colors.primary};
+  padding: 5px;
+  background-color: ${colors.light_grey};
+  border-radius: 3px;
+  align-items: center;
+  display: flex;
+  font-size: 1.2em;
 `;
 const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
-    color: ${colors.secondary};
-    &:hover {
-        color: ${colors.primary};
-        cursor: pointer;
-    }
+  color: ${colors.secondary};
+  &:hover {
+    color: ${colors.primary};
+    cursor: pointer;
+  }
 `;
 
 export default function Notification({
-    message,
-    visible,
-    handleHide,
-    duration = 8000,
+  message,
+  visible,
+  handleHide,
+  duration = 8000
 }) {
-    const props = useSpring({
-        transform: visible ? 'translateX(0px)' : 'translateX(100px)',
-        opacity: visible ? 1 : 0,
-    });
-    useEffect(() => {
-        let timeout;
-        if (visible === true) {
-            timeout = setTimeout(handleHide, duration);
-        }
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, [duration, handleHide, visible]);
-    return (
-        <Toast style={props}>
-            <p style={{ marginRight: '10px' }}>{message}</p>
-            <StyledFontAwesomeIcon onClick={() => handleHide()} icon="times" />
-        </Toast>
-    );
+  const props = useSpring({
+    transform: visible ? 'translateX(0px)' : 'translateX(100px)',
+    opacity: visible ? 1 : 0
+  });
+  useEffect(() => {
+    let timeout;
+    if (visible === true) {
+      timeout = setTimeout(handleHide, duration);
+    }
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [duration, handleHide, visible]);
+  return (
+    <Toast style={props}>
+      <p style={{ marginRight: '10px' }}>{message}</p>
+      <StyledFontAwesomeIcon onClick={() => handleHide()} icon="times" />
+    </Toast>
+  );
 }
