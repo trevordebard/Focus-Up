acdd15eacc9302143c6307255e92d08c374369db
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2019-06-07 17:39:05 -0500

    Refactor block/unblock sites.

--- public/electron.js
+++ public/electron.js
@@ -1,121 +1,121 @@
-const electron = require('electron');
+const electron = require("electron");
 
 const { app } = electron;
 const { BrowserWindow } = electron;
 const { ipcMain } = electron;
 const { dialog } = electron;
 
-const path = require('path');
-const isDev = require('electron-is-dev');
-const MenuBuilder = require('./menu');
-const BlockSites = require('./scripts/block_sites');
-const UnblockSites = require('./scripts/unblock_sites');
+const path = require("path");
+const isDev = require("electron-is-dev");
+const MenuBuilder = require("./menu");
+const BlockSites = require("./scripts/block_sites");
+const UnblockSites = require("./scripts/unblock_sites");
 
 let mainWindow;
 let blockInProgress = false;
 
 function confirmClose() {
-    dialog.showMessageBox(
-        {
-            type: 'question',
-            buttons: ['Yes', 'No'],
-            title: 'Confirm',
-            message:
-                'Are you sure you want to close in the middle of a Focus session?',
-        },
-        function(response) {
-            console.log(`response: ${response}`);
-            if (response === 0) {
-                // Runs the following if 'Yes' is clicked
-                app.showExitPrompt = false;
-                UnblockSites.unblock();
-                blockInProgress = false;
-                app.quit();
-            }
-        }
-    );
+  dialog.showMessageBox(
+    {
+      type: "question",
+      buttons: ["Yes", "No"],
+      title: "Confirm",
+      message:
+        "Are you sure you want to close in the middle of a Focus session?"
+    },
+    function(response) {
+      console.log(`response: ${response}`);
+      if (response === 0) {
+        // Runs the following if 'Yes' is clicked
+        app.showExitPrompt = false;
+        UnblockSites.unblock();
+        blockInProgress = false;
+        app.quit();
+      }
+    }
+  );
 }
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 400,
-        height: 422,
-        minWidth: 320,
-        titleBarStyle: 'hidden',
-        webPreferences: {
-            nodeIntegration: true,
-        },
-        icon: path.join(__dirname, 'favicon.ico'),
-    });
-    mainWindow.on('close', function(e) {
-        if (blockInProgress) {
-            e.preventDefault();
-            confirmClose();
-        } else {
-            app.quit();
-        }
-    });
-    mainWindow.loadURL(
-        isDev
-            ? 'http://localhost:3000'
-            : `file://${path.join(__dirname, '../build/index.html')}`
-    );
-    if (isDev) {
-        // Open the DevTools.
-        // BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-        // mainWindow.webContents.openDevTools();
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 422,
+    minWidth: 320,
+    titleBarStyle: "hidden",
+    webPreferences: {
+      nodeIntegration: true
+    },
+    icon: path.join(__dirname, "favicon.ico")
+  });
+  mainWindow.on("close", function(e) {
+    if (blockInProgress) {
+      e.preventDefault();
+      confirmClose();
+    } else {
+      app.quit();
     }
-    const menuBuilder = new MenuBuilder(mainWindow);
-    menuBuilder.buildMenu();
-    mainWindow.on('closed', () => (mainWindow = null));
+  });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  if (isDev) {
+    // Open the DevTools.
+    // BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    // mainWindow.webContents.openDevTools();
+  }
+  const menuBuilder = new MenuBuilder(mainWindow);
+  menuBuilder.buildMenu();
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 let frontEndSender = null;
-ipcMain.on('block', (event, sites) => {
-    frontEndSender = event.sender;
-    blockInProgress = true;
-    event.sender.send('load');
-    BlockSites.block(sites, ipcMain);
+ipcMain.on("block", (event, sites) => {
+  frontEndSender = event.sender;
+  blockInProgress = true;
+  event.sender.send("load");
+  BlockSites.block(sites, ipcMain);
 });
 
-ipcMain.on('unblock', () => {
-    blockInProgress = false;
-    UnblockSites.unblock();
+ipcMain.on("unblock", () => {
+  blockInProgress = false;
+  UnblockSites.unblock();
 });
 
-ipcMain.on('wwwListCreated', wwwList => {
-    frontEndSender.send('wwwListCreated', wwwList);
+ipcMain.on("blockComplete", wwwList => {
+  frontEndSender.send("blockComplete", wwwList);
 });
 
-ipcMain.on('updateProgress', data => {
-    frontEndSender.send('updateProgress', data);
+ipcMain.on("updateProgress", data => {
+  frontEndSender.send("updateProgress", data);
 });
-ipcMain.on('permissionDenied', () => {
-    frontEndSender.send('permissionDenied');
+ipcMain.on("permissionDenied", () => {
+  frontEndSender.send("permissionDenied");
 });
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', e => {
-    if (blockInProgress) {
-        e.preventDefault(); // Prevents the window from closing
-    } else {
-        app.quit();
-    }
+app.on("window-all-closed", e => {
+  if (blockInProgress) {
+    e.preventDefault(); // Prevents the window from closing
+  } else {
+    app.quit();
+  }
 });
 
 // Works with Command + Q
-app.on('before-quit', function(e) {
-    // Handle menu-item or keyboard shortcut quit here
-    if (blockInProgress) {
-        e.preventDefault();
-        confirmClose();
-    }
+app.on("before-quit", function(e) {
+  // Handle menu-item or keyboard shortcut quit here
+  if (blockInProgress) {
+    e.preventDefault();
+    confirmClose();
+  }
 });
 
-app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 exports.app = app;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,262 +1,261 @@
-import React, { Component, useState } from 'react';
-import styled from 'styled-components';
-import { Spinner, Alert } from '@blueprintjs/core';
-import http from 'http';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import CountdownInput from './CountdownInput';
-import Countdown from './Countdown';
-import Notification from './Notification';
-import colors from '../constants/colors';
-import db from '../constants/db';
+import React, { Component, useState } from "react";
+import styled from "styled-components";
+import { Spinner, Alert } from "@blueprintjs/core";
+import http from "http";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import CountdownInput from "./CountdownInput";
+import Countdown from "./Countdown";
+import Notification from "./Notification";
+import colors from "../constants/colors";
+import db from "../constants/db";
 
-const { ipcRenderer } = window.require('electron');
+const { ipcRenderer } = window.require("electron");
 
 const HomeContainer = styled.div`
-    display: block;
-    width: 80%;
-    margin: auto;
-    padding: 70px;
-    text-align: center;
+  display: block;
+  width: 80%;
+  margin: auto;
+  padding: 70px;
+  text-align: center;
 `;
 
 const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 const DefaultButton = styled.button`
-    color: ${colors.secondary};
-    font-size: 1em;
-    margin: 1em 0.5em;
-    padding: 0.6em 1em;
-    border: 2px solid ${colors.secondary};
-    border-radius: 3px;
-    min-width: 86px;
-    height: 40px;
-    background: linear-gradient(
-        to right,
-        ${colors.secondary} 0%,
-        ${colors.secondary} 50%,
-        #ffffff 50%,
-        #ffffff 100%
-    );
-    background-position: 100% 0;
-    background-size: 200% 100%;
-    transition: background-position 0.1s;
+  color: ${colors.secondary};
+  font-size: 1em;
+  margin: 1em 0.5em;
+  padding: 0.6em 1em;
+  border: 2px solid ${colors.secondary};
+  border-radius: 3px;
+  min-width: 86px;
+  height: 40px;
+  background: linear-gradient(
+    to right,
+    ${colors.secondary} 0%,
+    ${colors.secondary} 50%,
+    #ffffff 50%,
+    #ffffff 100%
+  );
+  background-position: 100% 0;
+  background-size: 200% 100%;
+  transition: background-position 0.1s;
 
-    :hover {
-        box-shadow: inset 0 0 0 2em adjust-hue(blue, 45deg);
-        color: #ffffff;
-        background-position: 0 0;
-        cursor: pointer;
-        .bp3-spinner-head {
-            stroke: white;
-        }
-    }
+  :hover {
+    box-shadow: inset 0 0 0 2em adjust-hue(blue, 45deg);
+    color: #ffffff;
+    background-position: 0 0;
+    cursor: pointer;
     .bp3-spinner-head {
-        stroke: ${colors.primary};
+      stroke: white;
     }
+  }
+  .bp3-spinner-head {
+    stroke: ${colors.primary};
+  }
 `;
 
 export default class Home extends Component {
-    interval = 0;
+  interval = 0;
 
-    constructor() {
-        super();
-        this.state = {
-            displayProgress: false,
-            time: 25,
-            timerStarted: false,
-            notificationText: 'Hello there',
-            notificationVisible: false,
-        };
-    }
-
-    componentDidMount() {
-        ipcRenderer.on('load', () => {
-            this.setState({ displayProgress: true });
-        });
-        ipcRenderer.on('wwwListCreated', (event, wwwList) => {
-            console.log('list created');
-            this.waitForBlock(wwwList);
-        });
-        ipcRenderer.on('permissionDenied', (event, data) => {
-            console.log('p denied');
-            this.setState({
-                displayProgress: false,
-                notificationText:
-                    'Permission denied. You must grant permission in order to start blocking.',
-                notificationVisible: true,
-            });
-            clearInterval(this.interval);
-        });
-    }
-
-    triggerBlock = () => {
-        const sites = db.get('blockedSites').value();
-        ipcRenderer.send('block', sites);
+  constructor() {
+    super();
+    this.state = {
+      displayProgress: false,
+      time: 25,
+      timerStarted: false,
+      notificationText: "Hello there",
+      notificationVisible: false
     };
+  }
 
-    waitForBlock = () => {
-        let blockedCount = 0;
+  componentDidMount() {
+    ipcRenderer.on("load", () => {
+      this.setState({ displayProgress: true });
+    });
+    ipcRenderer.on("blockComplete", (event, wwwList) => {
+      console.log("list created");
+      this.waitForBlock(wwwList);
+    });
+    ipcRenderer.on("permissionDenied", (event, data) => {
+      console.log("p denied");
+      this.setState({
+        displayProgress: false,
+        notificationText:
+          "Permission denied. You must grant permission in order to start blocking.",
+        notificationVisible: true
+      });
+      clearInterval(this.interval);
+    });
+  }
 
-        this.interval = setInterval(() => {
-            if (navigator.onLine) {
-                db.get('blockedSites')
-                    .value()
-                    .forEach(website => {
-                        http.get(`http://${website}`, () => {
-                            console.log(`${website} is not blocked`);
-                        }).on('error', () => {
-                            blockedCount += 1;
-                        });
-                    });
-            }
-            if (blockedCount === db.get('blockedSites').value().length) {
-                this.setState({
-                    displayProgress: false,
-                    timerStarted: true,
-                });
-                clearInterval(this.interval);
-            }
-        }, 3000);
-    };
+  triggerBlock = () => {
+    const sites = db.get("blockedSites").value();
+    ipcRenderer.send("block", sites);
+  };
 
-    triggerUnblock = () => {
-        ipcRenderer.send('unblock');
-    };
+  waitForBlock = () => {
+    let blockedCount = 0;
 
-    startCountdown = () => {
+    this.interval = setInterval(() => {
+      if (navigator.onLine) {
+        db.get("blockedSites")
+          .value()
+          .forEach(website => {
+            http
+              .get(`http://${website}`, () => {
+                console.log(`${website} is not blocked`);
+              })
+              .on("error", () => {
+                blockedCount += 1;
+              });
+          });
+      }
+      if (blockedCount === db.get("blockedSites").value().length) {
         this.setState({
-            timerStarted: true,
+          displayProgress: false,
+          timerStarted: true
         });
-    };
+        clearInterval(this.interval);
+      }
+    }, 3000);
+  };
 
-    stopCountdown = () => {
-        this.triggerUnblock();
-        this.setState({
-            timerStarted: false,
-        });
-    };
+  triggerUnblock = () => {
+    ipcRenderer.send("unblock");
+  };
 
-    setTime = value => {
-        // eslint-disable-next-line no-restricted-globals
-        if (isNaN(value)) {
-            this.setState({
-                notificationText: 'You may only enter numbers',
-                notificationVisible: true,
-            });
-        } else {
-            this.setState({ time: value, notificationVisible: false });
-        }
-    };
+  startCountdown = () => {
+    this.setState({
+      timerStarted: true
+    });
+  };
 
-    toggleTimerStarted = () => {
-        const { timerStarted } = this.state;
-        this.setState({ timerStarted: !timerStarted });
-    };
+  stopCountdown = () => {
+    this.triggerUnblock();
+    this.setState({
+      timerStarted: false
+    });
+  };
 
-    render() {
-        const {
-            displayProgress,
-            time,
-            timerStarted,
-            notificationText,
-            notificationVisible,
-        } = this.state;
-        return (
-            <HomeContainer>
-                {timerStarted ? (
-                    <div>
-                        <Countdown
-                            started={timerStarted}
-                            minutes={time}
-                            stopCountdown={this.stopCountdown}
-                        />
-                        <EndFocus
-                            triggerUnblock={this.triggerUnblock}
-                            toggleTimerStarted={this.toggleTimerStarted}
-                        />
-                    </div>
-                ) : (
-                    <div>
-                        <CountdownInput time={time} setTime={this.setTime} />
-                        <BeginFocus
-                            displayProgress={displayProgress}
-                            triggerBlock={this.triggerBlock}
-                        />
-                        <Link to="/blacklist">edit blacklist</Link>
-                    </div>
-                )}
-                <Notification
-                    message={notificationText}
-                    visible={notificationVisible}
-                    handleHide={() =>
-                        this.setState({
-                            notificationVisible: false,
-                        })
-                    }
-                    duration={8000}
-                />
-            </HomeContainer>
-        );
+  setTime = value => {
+    // eslint-disable-next-line no-restricted-globals
+    if (isNaN(value)) {
+      this.setState({
+        notificationText: "You may only enter numbers",
+        notificationVisible: true
+      });
+    } else {
+      this.setState({ time: value, notificationVisible: false });
     }
+  };
+
+  toggleTimerStarted = () => {
+    const { timerStarted } = this.state;
+    this.setState({ timerStarted: !timerStarted });
+  };
+
+  render() {
+    const {
+      displayProgress,
+      time,
+      timerStarted,
+      notificationText,
+      notificationVisible
+    } = this.state;
+    return (
+      <HomeContainer>
+        {timerStarted ? (
+          <div>
+            <Countdown
+              started={timerStarted}
+              minutes={time}
+              stopCountdown={this.stopCountdown}
+            />
+            <EndFocus
+              triggerUnblock={this.triggerUnblock}
+              toggleTimerStarted={this.toggleTimerStarted}
+            />
+          </div>
+        ) : (
+          <div>
+            <CountdownInput time={time} setTime={this.setTime} />
+            <BeginFocus
+              displayProgress={displayProgress}
+              triggerBlock={this.triggerBlock}
+            />
+            <Link to="/blacklist">edit blacklist</Link>
+          </div>
+        )}
+        <Notification
+          message={notificationText}
+          visible={notificationVisible}
+          handleHide={() =>
+            this.setState({
+              notificationVisible: false
+            })
+          }
+          duration={8000}
+        />
+      </HomeContainer>
+    );
+  }
 }
 
 const BeginFocus = props => {
-    let { displayProgress } = props;
-    const { triggerBlock } = props;
-    return (
-        <ButtonContainer>
-            <DefaultButton
-                onClick={() => {
-                    displayProgress = !displayProgress;
-                    triggerBlock();
-                }}
-                disabled={displayProgress}
-            >
-                {displayProgress ? <Spinner size={20} /> : 'Begin Focus'}
-            </DefaultButton>
-        </ButtonContainer>
-    );
+  let { displayProgress } = props;
+  const { triggerBlock } = props;
+  return (
+    <ButtonContainer>
+      <DefaultButton
+        onClick={() => {
+          displayProgress = !displayProgress;
+          triggerBlock();
+        }}
+        disabled={displayProgress}
+      >
+        {displayProgress ? <Spinner size={20} /> : "Begin Focus"}
+      </DefaultButton>
+    </ButtonContainer>
+  );
 };
 
 const EndFocus = props => {
-    const { toggleTimerStarted, triggerUnblock } = props;
-    const [alertOpen, setAlertOpen] = useState(false);
-    return (
-        <div>
-            <DefaultButton
-                onClick={() => setAlertOpen(true)}
-                disabled={alertOpen}
-            >
-                End Session Early
-            </DefaultButton>
-            <Alert
-                isOpen={alertOpen}
-                confirmButtonText="End Early"
-                cancelButtonText="Cancel"
-                intent="danger"
-                onCancel={() => setAlertOpen(false)}
-                onConfirm={() => {
-                    triggerUnblock();
-                    toggleTimerStarted();
-                }}
-                canOutsideClickCancel
-                transitionDuration={100}
-            >
-                <p>Are you sure you want to end this session early?</p>
-            </Alert>
-        </div>
-    );
+  const { toggleTimerStarted, triggerUnblock } = props;
+  const [alertOpen, setAlertOpen] = useState(false);
+  return (
+    <div>
+      <DefaultButton onClick={() => setAlertOpen(true)} disabled={alertOpen}>
+        End Session Early
+      </DefaultButton>
+      <Alert
+        isOpen={alertOpen}
+        confirmButtonText="End Early"
+        cancelButtonText="Cancel"
+        intent="danger"
+        onCancel={() => setAlertOpen(false)}
+        onConfirm={() => {
+          triggerUnblock();
+          toggleTimerStarted();
+        }}
+        canOutsideClickCancel
+        transitionDuration={100}
+      >
+        <p>Are you sure you want to end this session early?</p>
+      </Alert>
+    </div>
+  );
 };
 
 BeginFocus.propTypes = {
-    displayProgress: PropTypes.bool.isRequired,
-    triggerBlock: PropTypes.func.isRequired,
+  displayProgress: PropTypes.bool.isRequired,
+  triggerBlock: PropTypes.func.isRequired
 };
 EndFocus.propTypes = {
-    toggleTimerStarted: PropTypes.func.isRequired,
-    triggerUnblock: PropTypes.func.isRequired,
+  toggleTimerStarted: PropTypes.func.isRequired,
+  triggerUnblock: PropTypes.func.isRequired
 };
