4419c5cde172c814563fccadd4c9cd73d56bcf56
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2019-06-10 10:09:24 -0500

    Check for valid URL when adding item to Blacklist

--- src/components/Blacklist.js
+++ src/components/Blacklist.js
@@ -1,71 +1,71 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import db from '../constants/db';
-import colors from '../constants/colors';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import db from "../constants/db";
+import colors from "../constants/colors";
 
 library.add(faTrashAlt);
 
 const StyledList = styled.ul`
-    width: 50%;
-    font-size: 1.1em;
+  width: 50%;
+  font-size: 1.1em;
 `;
 const BlockedSite = styled.li`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    border-bottom: 1px solid ${colors.grey};
-    color: ${colors.secondary};
-    line-height: 1.7em;
-    :last-child {
-        border: none;
-    }
-    :hover {
-        background-color: ${colors.light_grey};
-    }
+  border-bottom: 1px solid ${colors.grey};
+  color: ${colors.secondary};
+  line-height: 1.7em;
+  :last-child {
+    border: none;
+  }
+  :hover {
+    background-color: ${colors.light_grey};
+  }
 `;
 
 const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
-    color: ${colors.grey};
-    &:hover {
-        color: ${colors.primary};
-        cursor: pointer;
-    }
+  color: ${colors.grey};
+  &:hover {
+    color: ${colors.primary};
+    cursor: pointer;
+  }
 `;
 
 const BlacklistContainer = styled.div`
-    padding: 10px;
-    margin: 40px 10px;
-    height: 100;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
+  padding: 10px;
+  margin: 40px 10px;
+  height: 100;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
 const InputContainer = styled.div`
-    display: relative;
+  display: relative;
 `;
 
 const SiteInput = styled.input`
-    box-shadow: 0 1px 3px hsla(0, 0%, 0%, 0.1);
-    font-size: 1em;
-    padding: 0.6em 1em;
-    color: ${colors.secondary};
-    ::placeholder {
-        color: ${colors.light_font};
-    }
-    :focus {
-        outline: none;
-    }
-    :hover {
-        box-shadow: 0 2px 5px hsla(0, 0%, 0%, 0.3);
-    }
-    border: 2px solid ${colors.secondary};
-    border-radius: 3px;
-    margin-right: 5px;
+  box-shadow: 0 1px 3px hsla(0, 0%, 0%, 0.1);
+  font-size: 1em;
+  padding: 0.6em 1em;
+  color: ${colors.secondary};
+  ::placeholder {
+    color: ${colors.light_font};
+  }
+  :focus {
+    outline: none;
+  }
+  :hover {
+    box-shadow: 0 2px 5px hsla(0, 0%, 0%, 0.3);
+  }
+  border: 2px solid ${colors.secondary};
+  border-radius: 3px;
+  margin-right: 5px;
 `;
 const DefaultButton = styled.button`
   color: ${colors.primary};
@@ -75,9 +75,7 @@ const DefaultButton = styled.button`
   padding: 0.6em 1em;
   border-radius: 3px;
   border: 2px solid ${colors.primary};
-  background: linear-gradient(to right, ${colors.primary} 0%,  ${
-    colors.primary
-} 50%, #ffffff 50%, #ffffff 100%);
+  background: linear-gradient(to right, ${colors.primary} 0%,  ${colors.primary} 50%, #ffffff 50%, #ffffff 100%);
   background-position: 100% 0;
   background-size: 200% 100%;
   transition: background-position 0.1s;
@@ -91,107 +89,105 @@ const DefaultButton = styled.button`
 `;
 
 const DefaultHeader = styled.h1`
-    color: ${colors.header};
-    font-weight: 10;
-    margin-bottom: 10px;
+  color: ${colors.header};
+  font-weight: 10;
+  margin-bottom: 10px;
 `;
 
 class Blacklist extends Component {
-    constructor() {
-        super();
-        this.state = {
-            sites: db.get('blockedSites').value(),
-            inputSiteValue: '',
-        };
+  constructor() {
+    super();
+    this.state = {
+      sites: db.get("blockedSites").value(),
+      inputSiteValue: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      inputSiteValue: event.target.value
+    });
+  };
+
+  addSiteToList = () => {
+    const { inputSiteValue } = this.state;
+    if (validURL(inputSiteValue)) {
+      db.get("blockedSites")
+        .push(inputSiteValue)
+        .write();
+      this.setState({ inputSiteValue: "" });
     }
+  };
 
-    renderSiteList = () => {
-        const { sites } = this.state;
-        if (sites.length !== 0) {
-            return sites.map(site => (
-                <p key={`${site}_${new Date().getTime()}`}>{site}</p>
-            ));
-        }
-        return null;
-    };
-
-    handleChange = event => {
-        this.setState({
-            inputSiteValue: event.target.value,
-        });
-    };
-
-    addSiteToList = () => {
-        const { inputSiteValue } = this.state;
-        db.get('blockedSites')
-            .push(inputSiteValue)
-            .write();
-        this.setState({ inputSiteValue: '' });
-    };
-
-    render() {
-        const { inputSiteValue, sites } = this.state;
-        const renderSites = sites.map(site => (
-            <BlockedSite key={`${site}_${new Date().getTime()}`}>
-                <p>{site}</p>
-                <StyledFontAwesomeIcon
-                    icon="trash-alt"
-                    id={site}
-                    value="test"
-                    onClick={e => {
-                        let item;
-                        if (
-                            e.target.parentNode.tagName.toLowerCase() === 'svg'
-                        ) {
-                            item = e.target.parentNode.id;
-                        } else if (
-                            e.target.parentNode.tagName.toLowerCase() === 'li'
-                        ) {
-                            item = e.target.id;
-                        }
-                        if (item !== null) {
-                            db.get('blockedSites')
-                                .pull(item)
-                                .write();
-                            this.forceUpdate();
-                        }
-                    }}
-                />
-            </BlockedSite>
-        ));
-        return (
-            <div>
-                <BlacklistContainer>
-                    <DefaultHeader>Blocked Sites</DefaultHeader>
-                    <StyledList>{renderSites}</StyledList>
-                    <br />
-                    <InputContainer>
-                        <SiteInput
-                            placeholder="example.com"
-                            value={inputSiteValue}
-                            onChange={this.handleChange}
-                        />
-                        <DefaultButton onClick={this.addSiteToList}>
-                            ADD
-                        </DefaultButton>
-                    </InputContainer>
-                    <br />
-                    <button
-                        style={{ display: 'none' }}
-                        type="button"
-                        onClick={() => {
-                            db.set('blockedSites', []).write();
-                            this.forceUpdate();
-                        }}
-                    >
-                        Clear Store
-                    </button>
-                    <Link to="/">Go back</Link>
-                    <br />
-                </BlacklistContainer>
-            </div>
-        );
-    }
+  render() {
+    const { inputSiteValue, sites } = this.state;
+    const renderSites = sites.map(site => (
+      <BlockedSite key={`${site}_${new Date().getTime()}`}>
+        <p>{site}</p>
+        <StyledFontAwesomeIcon
+          icon="trash-alt"
+          id={site}
+          value="test"
+          onClick={e => {
+            let item;
+            if (e.target.parentNode.tagName.toLowerCase() === "svg") {
+              item = e.target.parentNode.id;
+            } else if (e.target.parentNode.tagName.toLowerCase() === "li") {
+              item = e.target.id;
+            }
+            if (item !== null) {
+              db.get("blockedSites")
+                .pull(item)
+                .write();
+              this.forceUpdate();
+            }
+          }}
+        />
+      </BlockedSite>
+    ));
+    return (
+      <div>
+        <BlacklistContainer>
+          <DefaultHeader>Blocked Sites</DefaultHeader>
+          <StyledList>{renderSites}</StyledList>
+          <br />
+          <InputContainer>
+            <SiteInput
+              placeholder="example.com"
+              value={inputSiteValue}
+              onChange={this.handleChange}
+            />
+            <DefaultButton onClick={this.addSiteToList}>ADD</DefaultButton>
+          </InputContainer>
+          <br />
+          <button
+            style={{ display: "none" }}
+            type="button"
+            onClick={() => {
+              db.set("blockedSites", []).write();
+              this.forceUpdate();
+            }}
+          >
+            Clear Store
+          </button>
+          <Link to="/">Go back</Link>
+          <br />
+        </BlacklistContainer>
+      </div>
+    );
+  }
+}
+function validURL(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+    "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+    "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(str);
 }
 
 export default Blacklist;
